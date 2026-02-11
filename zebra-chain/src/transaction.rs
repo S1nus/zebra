366d43dc3e6d303069e3f5b9252635c94936dfdf
c-node <chuckhead7@gmail.com>
2026-02-11 18:36:34 -0500

    stubbed v7 variant and semantic verifier

--- zebra-chain/src/transaction.rs
+++ zebra-chain/src/transaction.rs
@@ -172,6 +172,30 @@ pub enum Transaction {
         /// The orchard data for this transaction, if any.
         orchard_shielded_data: Option<orchard::ShieldedData>,
     },
+    #[cfg(all(zcash_unstable = "ZFuture", feature = "tx_v7"))]
+    V7 {
+        /// The Network Upgrade for this transaction.
+        ///
+        /// Derived from the ConsensusBranchId field.
+        network_upgrade: NetworkUpgrade,
+        /// The earliest time or block height that this transaction can be added to the
+        /// chain.
+        lock_time: LockTime,
+        /// The latest block height that this transaction can be added to the chain.
+        expiry_height: block::Height,
+        /// The burn amount for this transaction, if any.
+        zip233_amount: Amount<NonNegative>,
+        /// The transparent inputs to the transaction.
+        inputs: Vec<transparent::Input>,
+        /// The transparent outputs from the transaction.
+        outputs: Vec<transparent::Output>,
+        /// The sapling shielded data for this transaction, if any.
+        sapling_shielded_data: Option<sapling::ShieldedData<sapling::SharedAnchor>>,
+        /// The orchard data for this transaction, if any.
+        orchard_shielded_data: Option<orchard::ShieldedData>,
+        /// The tachyon data for this transaction, if any.
+        tachyon_shielded_data: Option<tachyon::ShieldedData>,
+    }
 }
 
 impl fmt::Display for Transaction {
